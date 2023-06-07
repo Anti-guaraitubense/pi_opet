c5d63e3c0ac79beb69a760193a8a11cea4dc09fa
lgskrt <luizgmw2@gmail.com>
2023-06-07 14:46:15 -0300

    Início da aba principal

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt_BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,6 +19,45 @@
 </head>
 <body onload="evitar_dados_reload()";>
     <a href="#" class="logo">Doasans</a>
+    
+    <header id="header"> 
+        <nav class="navbar container">
+            <a href="#" class="logo"> 
+                <img src="#" alt="#" class="logo-image">
+            </a>
+            <button type="button" class="menu-toggle">
+                <i class="ri-menu-line toggle-icon open-menu-icon"></i>
+                <i class="ri-close-line toggle-icon close-menu-icon"></i>
+            </button>
+            <div class="collapsible-menu">
+                <ul class="list">
+                    <li class="list-item">
+                        <a href="#" class="list-link">Página Inicial</a>
+                    </li>
+                    <li class="list-item">
+                        <a href="#" class="list-link">Menu</a>
+                    </li>
+                    <li class="list-item">
+                        <a href="#" class="list-link">Serviços</a>
+                    </li>
+                    <li class="list-item">
+                        <a href="#" class="list-link">Comprar</a>
+                    </li>
+                </ul>
+                <div class="search-box">
+                    <form action="" class="search-form">
+                        <span class="form-icon search-icon">
+                           <!-- <sv></svg> -->
+                        </span>
+                        <input type="text" class="search-input" placeholder="Search">
+                        <button type="button" class="form-icon cart-icon">
+                            <!-- <sv></svg> -->
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </header>
 
     <?php 
 
