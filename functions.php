9d6f69b759be0d7747f6db21f91a6177b21dd524
Gustavo Walk <102780641+gustawalk@users.noreply.github.com>
2024-06-03 21:10:19 -0300

    check_cpf trocado podr loop

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php 
 
     function check_cpf($cpf_num){
-        
+            
         #transformando em array
         $cpf_array = array_map('intval', str_split($cpf_num));
 
@@ -9,12 +9,22 @@
             $lastdig1 = $cpf_array[9];
             $lastdig2 = $cpf_array[10];
 
-            $soma1 = $cpf_array[0] * 10 + $cpf_array[1] * 9 + $cpf_array[2] * 8 + $cpf_array[3] * 7 + $cpf_array[4] * 6 + $cpf_array[5] * 5 + $cpf_array[6] * 4 + $cpf_array[7] * 3 + $cpf_array[8] * 2;
+            $soma1 = 0;
+            
+            for($i=0;$i<=8;$i++){
+                $soma1+=$cpf_array[0+$i] * (10 - $i);
+            }
+            
             $soma1 *= 10;
             $soma1 %= 11;
 
             if($soma1 == $lastdig1){
-                $soma2 = $cpf_array[0] * 11 + $cpf_array[1] * 10 + $cpf_array[2] * 9 + $cpf_array[3] * 8 + $cpf_array[4] * 7 + $cpf_array[5] * 6 + $cpf_array[6] * 5 + $cpf_array[7] * 4 + $cpf_array[8] * 3 + $cpf_array[9] * 2;
+                
+                $soma2 = 0;
+                for($i=0;$i<=9;$i++){
+                    $soma2+=$cpf_array[0+$i] * (11-$i);
+                }
+                
                 $soma2 *= 10;
                 $soma2 %= 11;
 
